4e01c3c4ead2accba7f496333942f3b3f4d71298
ahmed elsman <elsman@u-smart.co>
2020-12-20 21:31:51 +0200

    Initial Commit

---
+++ iOSAssesment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSAssesment
+//
+//  Created by Ahmed Elsman on 20/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
